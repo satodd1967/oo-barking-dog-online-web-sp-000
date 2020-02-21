f0ae13dfd259f66f49cafb430e75ad97760aa0b4
Seth Todd <seth.todd@outlook.com>
2020-02-21 14:13:30 +0000

    Automatically backed up by Learn

--- spec/01_dogs_spec.rb
+++ spec/01_dogs_spec.rb
@@ -1,4 +1,4 @@
-ddescribe "Dog" do
+describe "Dog" do
   let(:fido) {Dog.new}  # Look up what let does in RSpec
   # https://www.relishapp.com/rspec/rspec-core/v/2-6/docs/helper-methods/let-and-let
 
